39fc19fcf5acaa5e3ff8da492dd88298b458bbe5
corneliuslaurenz <cornelius.laurenz@study.hs-duesseldorf.de>
2022-05-17 11:15:33 +0200

    Responsivness: GetInTouch, Overflow, Sidebar, Listenansicht

--- toolkit-frontity/packages/wireframe-theme/src/components/sidebar.js
+++ toolkit-frontity/packages/wireframe-theme/src/components/sidebar.js
@@ -1,56 +1,92 @@
-import React from "react"
-import { connect, styled } from "frontity"
-import Link from "@frontity/components/link"
+import React from "react";
+import { connect, styled } from "frontity";
+import Link from "@frontity/components/link";
 
-const Sidebar = ({state}) => {
-  
-    let currentPage = 0;
-    if(state.router.link.startsWith("/methodology/category/empathise")) {
-        currentPage = 1;
-    } else if(state.router.link.startsWith("/methodology/category/define")) {
-        currentPage = 2;
-    } else if(state.router.link.startsWith("/methodology/category/ideate")) {
-        currentPage = 3;
-    } else if(state.router.link.startsWith("/methodology/category/prototype")) {
-        currentPage = 4;
-    } else if(state.router.link.startsWith("/methodology/category/test")) {
-        currentPage = 5;
-    } else {
-        currentPage = 0;
-    }
+const Sidebar = ({ state }) => {
+  let currentPage = 0;
+  if (state.router.link.startsWith("/methodology/category/empathise")) {
+    currentPage = 1;
+  } else if (state.router.link.startsWith("/methodology/category/define")) {
+    currentPage = 2;
+  } else if (state.router.link.startsWith("/methodology/category/ideate")) {
+    currentPage = 3;
+  } else if (state.router.link.startsWith("/methodology/category/prototype")) {
+    currentPage = 4;
+  } else if (state.router.link.startsWith("/methodology/category/test")) {
+    currentPage = 5;
+  } else {
+    currentPage = 0;
+  }
 
+  return (
+    <>
+      <Main>
+        <nav>
+          <ul>
+            <li>
+              <Link
+                link={"/methodology/"}
+                aria-current={currentPage == 0 ? "page" : undefined}
+              >
+                Methoden Übersicht
+              </Link>
+            </li>
+            <li>
+              <Link
+                link={"/methodology/category/empathise/"}
+                aria-current={currentPage == 1 ? "page" : undefined}
+              >
+                Phase 1: Empathise
+              </Link>
+            </li>
+            <li>
+              <Link
+                link={"/methodology/category/define/"}
+                aria-current={currentPage == 2 ? "page" : undefined}
+              >
+                Phase 2: Define
+              </Link>
+            </li>
+            <li>
+              <Link
+                link={"/methodology/category/ideate/"}
+                aria-current={currentPage == 3 ? "page" : undefined}
+              >
+                Phase 3: Ideate
+              </Link>
+            </li>
+            <li>
+              <Link
+                link={"/methodology/category/prototype/"}
+                aria-current={currentPage == 4 ? "page" : undefined}
+              >
+                Phase 4: Prototype
+              </Link>
+            </li>
+            <li>
+              <Link
+                link={"/methodology/category/test/"}
+                aria-current={currentPage == 5 ? "page" : undefined}
+              >
+                Phase 5: Test
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </Main>
+    </>
+  );
+};
 
-
-    return (
-        <>
-            <Main>
-                <nav>
-                    <ul>
-                        <li><Link link={"/methodology/"} aria-current={currentPage==0  ? "page" : undefined}>Methoden Übersicht</Link></li>
-                        <li><Link link={"/methodology/category/empathise/"} aria-current={currentPage==1  ? "page" : undefined}>Phase 1: Empathise</Link></li>
-                        <li><Link link={"/methodology/category/define/"} aria-current={currentPage==2  ? "page" : undefined}>Phase 2: Define</Link></li>
-                        <li><Link link={"/methodology/category/ideate/"} aria-current={currentPage==3  ? "page" : undefined}>Phase 3: Ideate</Link></li>
-                        <li><Link link={"/methodology/category/prototype/"} aria-current={currentPage==4  ? "page" : undefined}>Phase 4: Prototype</Link></li>
-                        <li><Link link={"/methodology/category/test/"} aria-current={currentPage==5  ? "page" : undefined}>Phase 5: Test</Link></li>
-                    </ul>
-                </nav>
-            </Main>
-        </>
-    )
-}
-
-
-
-export default connect(Sidebar)
+export default connect(Sidebar);
 
 const Main = styled.div`
-position: absolute;
-margin-top: 70px;
-margin-left: 20px;
-border-right: 1px solid #C4C4C4;
-height: 700px;
-width: 300px;
-
+  position: absolute;
+  margin-top: 70px;
+  margin-left: 20px;
+  border-right: 1px solid #c4c4c4;
+  height: 700px;
+  width: 300px;
 
   nav {
     padding: 10px;
@@ -58,7 +94,6 @@ width: 300px;
 
   ul {
     list-style: none;
-
   }
 
   li > a {
@@ -74,14 +109,33 @@ width: 300px;
 
     /* Use for semantic approach to style the current link */
     &[aria-current="page"] {
-        color: #5A48F3;
-        opacity: 1;
-    }  
+      color: #5a48f3;
+      opacity: 1;
+    }
   }
 
   li > a:hover {
-    text-decoration: underline;  
+    text-decoration: underline;
   }
 
- 
-`
+  @media only screen and (max-width: 1580px) {
+    border-right: none;
+  }
+
+  @media only screen and (max-width: 1450px) {
+    position: relative;
+    margin-left: 0px;
+    margin-right: 0px;
+    height: 300px;
+    align-items: center;
+    text-align: center;
+    width: 100%;
+
+    nav {
+      padding-bottom: 20px !important;
+      border-bottom: 1px solid #c4c4c4 !important;
+      width: 80%;
+      margin: 0 auto;
+    }
+  }
+`;
