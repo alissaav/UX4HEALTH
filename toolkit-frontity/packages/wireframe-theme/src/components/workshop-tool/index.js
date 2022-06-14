352fb37c4bbe2107a1235b6e356113a7cc06ad5a
corneliuslaurenz <cornelius.laurenz@study.hs-duesseldorf.de>
2022-06-14 16:37:56 +0200

    Bugfix Auswahlfragen und Erstellung von 5 Auswahlfragen

--- toolkit-frontity/packages/wireframe-theme/src/components/workshop-tool/index.js
+++ toolkit-frontity/packages/wireframe-theme/src/components/workshop-tool/index.js
@@ -3,71 +3,146 @@ import React from "react";
 import Question from "./question";
 
 class WorkshopTool extends React.Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            currentScreen: 0,
-            question1: 0,
-            question2: 0,
-            question3: 0,
-        };
-
-        this.handleStateChange = this.handleStateChange.bind(this);
-        this.handlerQuestion1State = this.handlerQuestion1State.bind(this);
+    this.state = {
+      currentScreen: 0,
+      question1: 0,
+      question2: 0,
+      question3: 0,
+      question4: 0,
     };
 
-    renderSwitch() {
-        switch(this.state.currentScreen) {
-            case 0: 
-                return <div>    
-                    <h2>Brauchst du Unterstützung dabei, deinen Workshop zu planen?</h2> 
-                    <button onClick={() => {this.handleStateChange(1)}} >Ja!</button>
-                    <button onClick={() => {this.handleStateChange(2)}} >Dierekt zum Planungs-Tool</button>
-                </div>;
-            case 1: 
-                return <div>
-                            <Question question="Was ist das Ziel Ihres Workshops?" answers={["Produktentwicklung", "Produkt testen", "Ideensammlung"]} answerHandler={this.handlerQuestion1State}/>
-                            {   
-                                
-                                this.state.question1 != 0 && <div> Hi</div>
-                            }
-                        </div>;
-            case 2: 
-                return <div>
-                    Goodbye!
-                </div>;
-        }
-    }
+    this.handleStateChange = this.handleStateChange.bind(this);
+    this.handlerQuestion1State = this.handlerQuestion1State.bind(this);
+  }
 
-    handleStateChange(number){
-        this.setState({currentScreen: number});
-    }
-
-    handlerQuestion1State(value) {
-        this.setState({question1: value});
-    }
-
-    render() {
+  renderSwitch() {
+    switch (this.state.currentScreen) {
+      case 0:
         return (
-            <WorkshopContainer>
-                {
-                    this.renderSwitch()
-                }
-            </WorkshopContainer>
-        
-        )
-        
+          <div>
+            <h2>Brauchst du Unterstützung dabei, deinen Workshop zu planen?</h2>
+            <button
+              onClick={() => {
+                this.handleStateChange(1);
+              }}
+            >
+              Ja!
+            </button>
+            <button
+              onClick={() => {
+                this.handleStateChange(2);
+              }}
+            >
+              Direkt zum Planungs-Tool
+            </button>
+          </div>
+        );
+      case 1:
+        return (
+          <div>
+            <Question
+              question="Was ist das Ziel Ihres Workshops?"
+              answers={[
+                "Produktentwicklung",
+                "Produkt testen",
+                "Ideensammlung",
+              ]}
+              answerHandler={this.handlerQuestion1State.bind(this)}
+            />
+            {this.state.question1 != 0 && (
+              <div>
+                <Question
+                  question="Was ist die Anzahl der Teilnehmer?"
+                  answers={["5-10", "10-15", "15-25", ">25"]}
+                  answerHandler={this.handlerQuestion2State.bind(this)}
+                />
+              </div>
+            )}
+            {this.state.question2 != 0 && (
+              <div>
+                <Question
+                  question="Soll der Workshop online oder in Präsenz stattfinden?"
+                  answers={["Online", "Präsenz"]}
+                  answerHandler={this.handlerQuestion3State.bind(this)}
+                />
+              </div>
+            )}
+            {this.state.question3 != 0 && (
+              <div>
+                <Question
+                  question="Was ist die Anzahl der Teilnehmer?"
+                  answers={["5-10", "10-15", "15-25", ">25"]}
+                  answerHandler={this.handlerQuestion4State.bind(this)}
+                />
+              </div>
+            )}
+            {this.state.question4 != 0 && (
+              <div>
+                <Question
+                  question="Wie lange soll Ihr Workshop gehen?"
+                  answers={["3-4 Stunden", "6-8 Stunden", "2 Tage"]}
+                  answerHandler={this.handlerFinalQuestion.bind(this)}
+                />
+              </div>
+            )}
+          </div>
+        );
+      case 2:
+        return (
+          <div className="pt">Herzlich Wilkommen auf dem Planungstool!</div>
+        );
     }
+  }
 
+  handleStateChange(number) {
+    this.setState({ currentScreen: number });
+  }
 
+  handlerQuestion1State(value) {
+    this.setState({ question1: value });
+  }
+
+  handlerQuestion2State(value) {
+    this.setState({ question2: value });
+  }
+
+  handlerQuestion3State(value) {
+    this.setState({ question3: value });
+  }
+
+  handlerQuestion4State(value) {
+    this.setState({ question4: value });
+  }
+
+  handlerFinalQuestion() {
+    this.setState({ currentScreen: 2 });
+  }
+
+  render() {
+    return <WorkshopContainer>{this.renderSwitch()}</WorkshopContainer>;
+  }
 }
 export default connect(WorkshopTool);
 
-const WorkshopContainer = styled.div `
-    margin-top: 100px;
+const WorkshopContainer = styled.div`
+  margin-top: 140px;
 
-    button {
-        padding: 20px;
-    }
-`
+  div {
+    items-align: center;
+    text-align: center;
+  }
+
+  .pt {
+    margin-bottom: 412px;
+  }
+
+  button {
+    width: 160px;
+    padding: 20px;
+    margin: 20px;
+    margin-bottom: 150px !important;
+  }
+`;
--- toolkit-frontity/packages/wireframe-theme/src/components/workshop-tool/question.js
+++ toolkit-frontity/packages/wireframe-theme/src/components/workshop-tool/question.js
@@ -1,32 +1,40 @@
 import React from "react";
 import { connect, styled } from "frontity";
 
-
 class Question extends React.Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.handleClick = this.handleClick.bind(this);
-    }
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    
+  render() {
+    return (
+      <div>
+        <h2> {this.props.question} </h2>
+        {
+          //this.props.answerHandler(this.props.answers[1])
+          //this.props.answers[0];
+        }
+        {this.props.answers.map((item, i) => {
+          return (
+            <button
+              key={i + 1}
+              onClick={() => {
+                this.handleClick(i + 1);
+              }}
+            >
+              {item}
+            </button>
+          );
+        })}
+      </div>
+    );
+  }
 
-    render() {
-        return (
-            <div>
-                <h2> {this.props.question} </h2>
-                {
-                    this.props.answers.map(function(item, i){
-                            return <button key={i} onClick={() => {this.handleClick(i)}}>{item}</button>
-                        })
-                }
-            </div>
-        );
-    }
-
-    handleClick(index) {
-        this.props.answerHandler(index);
-    }
+  handleClick(index) {
+    this.props.answerHandler(index);
+  }
 }
 
 export default connect(Question);
