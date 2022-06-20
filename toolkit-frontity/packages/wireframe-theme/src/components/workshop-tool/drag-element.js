c66b1d92083007dab76ba24405ee596315a136f8
Alissa Avtonoshkina <alissa.avtonoshkina@study.hs-duesseldorf.de>
2022-06-20 15:02:59 +0200

    drag & drop test

---
+++ toolkit-frontity/packages/wireframe-theme/src/components/workshop-tool/drag-element.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useDrag } from 'react-dnd';
+
+class DragElement extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return 
+            <>
+                
+            </>
+    }
+}
